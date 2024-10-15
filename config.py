691f03bb022106097a342d3718cd04954c1e355c
WronaAcc <holahfk@gmail.com>
2024-10-15 11:59:08 +0100

    PUSH

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'AB22CC95'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://root:Void240901!@localhost/kino_rezerwacje'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'YOURSECRETKEY'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://root:YOUR_ROOT_PASSWORD@localhost/kino_rezerwacje'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Flask-Mail configuration
-    MAIL_SERVER = 'poczta.o2.pl'
-    MAIL_PORT = 465
+    MAIL_SERVER = 'YOUR_MAIL_SERVER'
+    MAIL_PORT = 'YOUR_MAIL_PORT'
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'Karlosss123@o2.pl'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'RGKNZTBUHUC2HWC5'
-    ADMINS = ['Karlosss123@o2.pl']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'YOUR_MAIL_USERNAME'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'YOUR_MAIL_PASSWORD'
+    ADMINS = ['YOUR_ADMINS']
     WTF_CSRF_ENABLED = True
